aca48a18dd50575e2b0b79f69ba4c5a38409e8cc
geoffroy <you@example.com>
2014-09-22 16:47:25 +0200

    deb for cf-cli and support for initial clones

---
+++ start_c9.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+set -x
+
+TMP=`pwd`
+for i in ${CLONES}
+do
+    REPO=`basename ${i} '.git'`
+    if [ -d "${HOME}/workspace/${REPO}/.git" ]; then
+	echo "The repository '${HOME}/workspace/${REPO}' already exists"
+    else
+	cd "$HOME/workspace" && git clone "$i"
+    fi
+done
+cd "$TMP"
+
+set +x
+
+
+if [ -z "${C9PORT}" ]; then 
+    PORT=""
+else 
+    PORT="-p ${C9PORT}"
+fi
+
+
+bin/cloud9.sh -l 0.0.0.0 $PORT --username "$C9USERNAME" --password "$C9PASSWORD" -w "$HOME/workspace"
